a60fccf3e5a9398edd76895abbf3e649fd8d1e18
tim58786 <tim58786@gmail.com>
2024-05-29 13:59:01 +0800

    07

---
+++ js/Member.js
@@ -0,0 +1,32 @@
+
+function join(){
+    window.location.href="Members join-1.html";
+}
+
+// function Password(password) {
+//     // 密碼至少需要包含8個字符
+//     if (password.length < 8) {
+//         return false;
+//     }
+    
+//     // 密碼中至少需要包含一個大寫字母、一個小寫字母和一個數字
+//     var hasUpperCase = /[A-Z]/.test(password);
+//     var hasLowerCase = /[a-z]/.test(password);
+//     var hasNumber = /\d/.test(password);
+    
+//     if (!hasUpperCase || !hasLowerCase || !hasNumber) {
+//         return false;
+//     }
+    
+//     // 如果密碼符合所有標準，返回true
+//     return true;
+// }
+
+
+
+
+
+
+
+
+  
